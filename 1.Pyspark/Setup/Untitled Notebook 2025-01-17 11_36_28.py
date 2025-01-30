77dffd7e23277744bde223a226c1e60fd051b7a9
manoj4043 <165779901+manoj4043@users.noreply.github.com>
2025-01-30 16:47:38 +0530

    Delete 1.Pyspark/Setup/Untitled Notebook 2025-01-17 11_36_28.py

--- 1.Pyspark/Setup/Untitled Notebook 2025-01-17 11_36_28.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC drop database f1_processed cascade
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC create database f1_processed
-# MAGIC managed location "/mnt/vasanthblob/processed"
